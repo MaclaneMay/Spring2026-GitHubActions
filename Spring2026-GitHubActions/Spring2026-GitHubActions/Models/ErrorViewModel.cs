3eee782c9dafad1d910373a68948a0aa878e2ff1
Maclane May <may@cs.ua.edu>
2026-04-09 14:01:45 -0500

    - Init solution commit

---
+++ Spring2026-GitHubActions/Spring2026-GitHubActions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_GitHubActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
